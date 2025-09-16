216cb5bd87cdedd2d8e99c54143fc05a10bd9914
sihaoz826 <sihaoz@andrew.cmu.edu>
2025-09-16 14:35:16 -0400

    changed default flask forge numbers

--- albumy/__init__.py
+++ albumy/__init__.py
@@ -122,12 +122,12 @@ def register_commands(app):
         click.echo('Done.')
 
     @app.cli.command()
-    @click.option('--user', default=10, help='Quantity of users, default is 10.')
-    @click.option('--follow', default=30, help='Quantity of follows, default is 30.')
-    @click.option('--photo', default=30, help='Quantity of photos, default is 30.')
-    @click.option('--tag', default=20, help='Quantity of tags, default is 20.')
-    @click.option('--collect', default=50, help='Quantity of collects, default is 50.')
-    @click.option('--comment', default=100, help='Quantity of comments, default is 100.')
+    @click.option('--user', default=5, help='Quantity of users, default is 5.')
+    @click.option('--follow', default=10, help='Quantity of follows, default is 10.')
+    @click.option('--photo', default=10, help='Quantity of photos, default is 10.')
+    @click.option('--tag', default=10, help='Quantity of tags, default is 10.')
+    @click.option('--collect', default=20, help='Quantity of collects, default is 20.')
+    @click.option('--comment', default=30, help='Quantity of comments, default is 30.')
     def forge(user, follow, photo, tag, collect, comment):
         """Generate fake data."""
 
